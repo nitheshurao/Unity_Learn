5afbe609abe6ebb22dfd6ef5b975817cf2d90eb8
Rithesh <nitheshurao@gmail.com>
2021-06-09 20:37:37 +0530

    Seen managment

---
+++ Learn_Unity/Assets/UI/Scripts/UIScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float resX;
+    public float rexY;
+    private CanvasScaler cas;
+
+    private void Start()
+    {
+        cas = GetComponent<CanvasScaler>();
+        SetInfo();
+
+    }
+    void SetInfo()
+    {
+        resX = (float)Screen.currentResolution.width;
+        rexY = (float)Screen.currentResolution.height;
+
+        cas.referenceResolution = new Vector2(resX, rexY);
+    }
+}
