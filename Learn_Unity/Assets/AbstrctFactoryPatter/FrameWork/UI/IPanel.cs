21165a55cb2fe31f984ef82ea57cf0508ee27b27
Rithesh <nitheshurao@gmail.com>
2021-06-14 22:26:15 +0530

    Patter
    Using default Factory patter

---
+++ Learn_Unity/Assets/AbstrctFactoryPatter/FrameWork/UI/IPanel.cs
@@ -0,0 +1,12 @@
+public interface IPanel
+{
+	/// <summary>
+	/// Activate the Panel
+	/// </summary>
+	void Activate();
+
+	/// <summary>
+	/// Deactivate the Panel
+	/// </summary>
+	void Deactivate();
+}
